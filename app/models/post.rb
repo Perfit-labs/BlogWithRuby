70ca46e0e325084f3da37b8cbb848fb15a43d498
Austin Vern Songer <austinvernsonger@outlook.com>
2016-12-30 02:33:07 +0000

    add validates to post.rb and comments.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,4 @@
-class Post < ApplicationRecord
+class Post < ActiveRecord::Base
+  has_many :comments
+  validates :title, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161230015915) do
+ActiveRecord::Schema.define(version: 20161230022132) do
 
   create_table "admin_uses", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
